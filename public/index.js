2c2fbafb2877f63be44c710db9189de0b3f3a595
Frank Buono <fbuonojr@fordham.edu>
2020-09-21 21:44:46 -0400

    added service worker to index

--- public/index.js
+++ public/index.js
@@ -1,3 +1,12 @@
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('/service-worker.js')
+      .then((register) => {
+        console.log('Service worker registered.', register)
+      })
+  })
+}
+
 let transactions = [];
 let myChart;
 
